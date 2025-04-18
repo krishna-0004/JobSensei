92ff9a1f8e24aa194259fe10db4ffa95c05987e9
Krishna Kadukar <krishnakadukar.skillolite@gmail.com>
2025-04-18 20:24:32 +0530

    ntg

--- backend/middleware/jwt.mjs
+++ backend/middleware/jwt.mjs
@@ -7,6 +7,8 @@ export const authenticateJWT = (req, res, next) => {
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
+
+    
     req.user = user;
     next();
   });
